65a7ebbbe11342c44d9b3e71e4c0fc3288747683
StraightStrike <justinzhang@vt.edu>
2023-10-22 18:05:52 -0400

    reload on start

--- Assets/Script/SniperControl.cs
+++ Assets/Script/SniperControl.cs
@@ -25,6 +25,8 @@ public class SniperControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        reloadCountDown = reloadTime;
+        Invoke("Reload", 0.5f);
     }
 
     // Update is called once per frame
